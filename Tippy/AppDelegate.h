a5a858cc50cef3c1d2873ef14a52d9f9c3404be5
Haley Zeng <hhzeng@fb.com>
2018-06-26 13:32:19 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Haley Zeng on 6/26/18.
+//  Copyright © 2018 Haley Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tippy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tippy
+//
+//  Created by Haley Zeng on 6/26/18.
+//  Copyright © 2018 Haley Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
